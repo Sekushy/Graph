3ce2e48e2697c5b3323521daaa07083617c96ba5
Drăghici Andrei <35525357+Sekushy@users.noreply.github.com>
2019-07-05 09:47:28 +0300

    Added Dijkstras algorithm

--- Graph.py
+++ Graph.py
@@ -1,5 +1,5 @@
 #!usr/bin/env python3
-from collections import defaultdict
+from collections import defaultdict, deque
 
 class Graph(object):
     def __init__(self):
@@ -14,9 +14,54 @@ class Graph(object):
         self.edges[from_node].append(to_node)
         self.edges[to_node].append(from_node)
         self.distances[(from_node, to_node)] = distance
+        self.distances[(to_node, from_node)] = distance
     
     def get_edges(self):
         return len(self.edges)
     
     def print_graph(self):
         print([vertex for vertex in self.vertices])
+    
+    def dijkstra(self, origin):
+        visited = {origin : 0}
+        path = {}
+        vertices = set(self.vertices)
+
+        while vertices:
+            minimum = None
+            for vertex in vertices:
+                if vertex in visited:
+                    if minimum is None:
+                        minimum = vertex
+                    elif visited[vertex] < visited[minimum]:
+                        minimum = vertex
+            if minimum is None:
+                break
+
+            vertices.remove(minimum)
+            current_weight = visited[minimum]
+
+            for edge in self.edges[minimum]:
+                try:
+                    weight = current_weight + self.distances[(minimum, edge)]
+                except:
+                    continue
+                if edge not in visited or weight < visited[edge]:
+                    visited[edge] = weight
+                    path[edge] = minimum
+        return visited, path
+
+    def get_shortest_path(self, origin, destination):
+        visited, paths = self.dijkstra(origin)
+        full_path = deque()
+        
+        _destination = paths[destination]
+
+        while _destination != origin:
+            full_path.appendleft(_destination)
+            _destination = paths[_destination]
+
+        full_path.appendleft(origin)
+        full_path.append(destination)
+
+        return visited[destination], list(full_path)
--- main.py
+++ main.py
@@ -5,6 +5,29 @@ if __name__ == "__main__":
     graph = Graph()
     for vertex in ['London', 'Brussels', 'Paris', 'Granada', 'Milan', 'Rome', 'Vienna', 'Athens', 'Oslo', 'Kiev', 'Stockholm', 'Helsinki']:
         graph.add_vertex(vertex)
-    
-    graph.print_graph()
-    print(graph.get_edges())
+
+    # London
+    graph.add_edge('London', 'Brussels', 50)
+    graph.add_edge('London', 'Paris', 5)
+    graph.add_edge('London', 'Granada', 78)
+    # Granada
+    graph.add_edge('Granada', 'Paris', 30)
+    graph.add_edge('Granada', 'Milan', 50)
+    graph.add_edge('Granada', 'Athens', 81)
+    # Athens
+    graph.add_edge('Athens', 'Vienna', 13)
+    # Vienna
+    graph.add_edge('Vienna', 'Rome', 10)
+    graph.add_edge('Vienna', 'Brussels', 16)
+    graph.add_edge('Vienna', 'Kiev', 0)
+    graph.add_edge('Vienna', 'Stockholm', 1)
+    # Stockholm
+    graph.add_edge('Stockholm', 'Helsinki', 0)
+    # Helsinki
+    graph.add_edge('Helsinki', 'Oslo', 12)
+    # Oslo
+    graph.add_edge('Oslo', 'Brussels', 35)
+    # Milan
+    graph.add_edge('Milan', 'Rome', 1)
+
+    print(graph.get_shortest_path('London', 'Kiev'))
